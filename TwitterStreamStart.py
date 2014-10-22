69b55efcbf0dfefbb42e05d5820fb9fefbb20ee4
errolgrannum <errolgrannum@gmail.com>
2014-10-21 21:17:21 -0400

    add simple tweet parsing functionality

--- TwitterStreamStart.py
+++ TwitterStreamStart.py
@@ -1,15 +1,28 @@
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+import time
+
 
 #import project keys + other project configuration data
 from config import *
 
 
+def sentimentAnalysis(text):
+	pass
+
+
 class listener (StreamListener):
 
 	def on_data(self, data):
-		print data
+		tweet = data.split(',"text":"')[1].split('","source')[0]
+		sentimentRating = sentimentAnalysis(tweet)
+
+		save = tweet + '::' + sentimentRating +'\n'
+		output = open('output.csv', 'a')
+		output.write(save)
+		output.close()
+		# print data
 		return True
 
 	def on_error(self, status):
@@ -18,5 +31,6 @@ class listener (StreamListener):
 # we need to authorize ourselves
 auth = OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
+
 twitterStream = Stream(auth,listener())
 twitterStream.filter(track=["Howard,University"])
